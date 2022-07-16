c1cd666ae5bcfa7c03422906036b8b984e8e4885
Jbargotio <jbargotio@gmail.com>
2022-07-15 23:58:55 -0700

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+  
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, min_idx;
+  
+    /* One by one move boundary of unsorted subarray*/
+    for (i = 0; i < size-1; i++)
+    {
+        /* Find the minimum element in unsorted array*/
+        min_idx = i;
+        for (j = i+1; j < size; j++)
+          if (array[j] < array[min_idx])
+		  min_idx = j;
+	swap(&array[min_idx], &array[i]);
+	print_array(array, size);
+    }
+}
