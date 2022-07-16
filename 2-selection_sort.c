409534f5323def76027185235de7d8da0cbbe8a5
Jbargotio <jbargotio@gmail.com>
2022-07-16 00:05:01 -0700

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,25 +1,45 @@
 #include "sort.h"
 
-void swap(int *xp, int *yp)
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
-  
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
-  
-    /* One by one move boundary of unsorted subarray*/
-    for (i = 0; i < size-1; i++)
-    {
-        /* Find the minimum element in unsorted array*/
-        min_idx = i;
-        for (j = i+1; j < size; j++)
-          if (array[j] < array[min_idx])
-		  min_idx = j;
-	swap(&array[min_idx], &array[i]);
-	print_array(array, size);
-    }
+	int *min;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
+		{
+			swap_ints(array + i, min);
+			print_array(array, size);
+		}
+	}
 }
